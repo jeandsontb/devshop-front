8f903f49d46548ac2604bf7097e91d3099b8743a
jeandson <jeandsontb@gmail.com>
2022-08-19 20:01:43 -0300

    implements upload front

--- panel/hooks/graphql/index.tsx
+++ panel/hooks/graphql/index.tsx
@@ -45,4 +45,48 @@ const useMutation = (query: any) => {
   return [data, mutate];
 };
 
-export { useQuery, useMutation, fetcher };
+const uploader = async (formData: any) => {
+  const res = await fetch(
+    process.env.NEXT_PUBLIC_API ? process.env.NEXT_PUBLIC_API : "",
+    {
+      headers: {},
+      method: "POST",
+      body: formData,
+    }
+  );
+
+  const json = await res.json();
+  return json;
+};
+
+const useUpload = (query: any) => {
+  const [data, setData] = useState<any>(null);
+  const mutate = async (variables: any) => {
+    const mutation = {
+      query,
+      variables: {
+        ...variables,
+        file: null,
+      },
+    };
+
+    const map = {
+      "0": ["variables.file"],
+    };
+
+    const formData = new FormData();
+    formData.append("operations", JSON.stringify(mutation));
+    formData.append("map", JSON.stringify(map));
+    formData.append("0", variables.file);
+
+    try {
+      const returnData = await uploader(formData);
+      setData(returnData);
+      return returnData;
+    } catch (err) {}
+  };
+
+  return [data, mutate];
+};
+
+export { useQuery, useMutation, fetcher, useUpload };
