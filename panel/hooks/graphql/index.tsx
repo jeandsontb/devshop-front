5d9af44e29b072e989e97c08c9760d0b917ba59a
jeandson <jeandsontb@gmail.com>
2022-08-31 21:50:49 -0300

    implements front adjusts

--- panel/hooks/graphql/index.tsx
+++ panel/hooks/graphql/index.tsx
@@ -1,12 +1,43 @@
 import { useState } from "react";
 import useSWR from "swr";
 
+const getNewAccessToken = async () => {
+  const accessToken = localStorage.getItem("accessToken");
+  const getAccessToken = {
+    query: `
+      mutation accessToken($refreshToken: String!) {
+        accessToken(refreshToken: $refreshToken)
+      }
+    `,
+    variables: {
+      refreshToken: localStorage.getItem("refreshToken"),
+    },
+  };
+
+  const resAccessToken = await fetch(
+    process.env.NEXT_PUBLIC_API ? process.env.NEXT_PUBLIC_API : "",
+    {
+      headers: {
+        "Content-type": "application/json",
+        authorization: accessToken ?? "Bearer " + accessToken,
+      },
+      method: "POST",
+      body: JSON.stringify(getAccessToken),
+    }
+  );
+  const jsonAccessToken = await resAccessToken.json();
+
+  return jsonAccessToken;
+};
+
 const fetcher = async (query: any) => {
+  const accessToken = localStorage.getItem("accessToken");
   const res = await fetch(
     process.env.NEXT_PUBLIC_API ? process.env.NEXT_PUBLIC_API : "",
     {
       headers: {
         "Content-type": "application/json",
+        authorization: accessToken ?? "Bearer " + accessToken,
       },
       method: "POST",
       body: query,
@@ -14,7 +45,89 @@ const fetcher = async (query: any) => {
   );
 
   const json = await res.json();
-  return json;
+  if (
+    !(
+      json.errors &&
+      json.errors[0] &&
+      json.errors[0].message === "Forbidden resource"
+    )
+  ) {
+    return json;
+  }
+
+  //pegar um novo accessToken
+  const jsonAccessToken = await getNewAccessToken();
+  if (jsonAccessToken.data) {
+    const newAccessToken = jsonAccessToken.data.accessToken;
+    localStorage.setItem("accessToken", newAccessToken);
+
+    const res2 = await fetch(
+      process.env.NEXT_PUBLIC_API ? process.env.NEXT_PUBLIC_API : "",
+      {
+        headers: {
+          "Content-type": "application/json",
+          authorization: "Bearer " + newAccessToken,
+        },
+        method: "POST",
+        body: query,
+      }
+    );
+
+    const json2 = await res2.json();
+    if (!json2.errors) {
+      return json2;
+    }
+  }
+
+  // enviar para login
+  window.location.href = "/";
+  return null;
+};
+
+const uploader = async (formData: any) => {
+  const accessToken = localStorage.getItem("accessToken");
+  const res = await fetch(
+    process.env.NEXT_PUBLIC_API ? process.env.NEXT_PUBLIC_API : "",
+    {
+      headers: {
+        authorization: "Bearer " + accessToken,
+      },
+      method: "POST",
+      body: formData,
+    }
+  );
+
+  const json = await res.json();
+  if (!json) {
+    return json;
+  }
+
+  //pegar um novo accessToken
+  const jsonAccessToken = await getNewAccessToken();
+  if (jsonAccessToken.data) {
+    const newAccessToken = jsonAccessToken.data.accessToken;
+    localStorage.setItem("accessToken", newAccessToken);
+
+    const res2 = await fetch(
+      process.env.NEXT_PUBLIC_API ? process.env.NEXT_PUBLIC_API : "",
+      {
+        headers: {
+          authorization: "Bearer " + newAccessToken,
+        },
+        method: "POST",
+        body: formData,
+      }
+    );
+
+    const json2 = await res2.json();
+    if (!json2.errors) {
+      return json2;
+    }
+  }
+
+  // enviar para login
+  window.location.href = "/";
+  return null;
 };
 
 const useQuery = (queryString: any) => {
@@ -43,20 +156,6 @@ const useMutation = (query: any) => {
   };
 
   return [data, mutate];
-};
-
-const uploader = async (formData: any) => {
-  const res = await fetch(
-    process.env.NEXT_PUBLIC_API ? process.env.NEXT_PUBLIC_API : "",
-    {
-      headers: {},
-      method: "POST",
-      body: formData,
-    }
-  );
-
-  const json = await res.json();
-  return json;
 };
 
 const useUpload = (query: any) => {
